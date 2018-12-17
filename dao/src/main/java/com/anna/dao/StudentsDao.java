e0d5f6f6f38a9daaa8c29ea4d39a835b2e8ffd0e
Hanna Hlushchanka <Hanna_Hlushchanka@epam.com>
2018-12-17 18:34:37 +0300

    implement studentsDao

--- dao/src/main/java/com/anna/dao/StudentsDao.java
+++ dao/src/main/java/com/anna/dao/StudentsDao.java
@@ -1,7 +1,30 @@
 package com.anna.dao;
 
+import com.anna.model.Group;
+import com.anna.model.Student;
+
 import java.util.List;
 
 public interface StudentsDao {
+
+    List<Group> getGroups(Integer minStudent, Integer maxStudent);
+
+    Group getGroupById();
+
+    Integer addGroup(Group group);
+
+    Integer updateGroup(Group group);
+
+    Integer deleteGroup(Integer id);
+
+    List<Student> getStudents(Integer minAge, Integer maxAge);
+
+    Student getStudentById();
+
+    Integer addStudent(Student student);
+
+    Integer updateStudent(Student student);
+
+    Integer deleteStudent(Integer id);
 
 }
---
+++ dao/src/main/java/com/anna/dao/StudentsDaoImpl.java
@@ -0,0 +1,62 @@
+package com.anna.dao;
+
+import com.anna.model.Group;
+import com.anna.model.Student;
+
+import java.util.List;
+
+public class StudentsDaoImpl implements StudentsDao {
+
+
+
+
+    @Override
+    public List<Group> getGroups(Integer minStudent, Integer maxStudent) {
+        return null;
+    }
+
+    @Override
+    public Group getGroupById() {
+        return null;
+    }
+
+    @Override
+    public Integer addGroup(Group group) {
+        return null;
+    }
+
+    @Override
+    public Integer updateGroup(Group group) {
+        return null;
+    }
+
+    @Override
+    public Integer deleteGroup(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Student> getStudents(Integer minAge, Integer maxAge) {
+        return null;
+    }
+
+    @Override
+    public Student getStudentById() {
+        return null;
+    }
+
+    @Override
+    public Integer addStudent(Student student) {
+        return null;
+    }
+
+    @Override
+    public Integer updateStudent(Student student) {
+        return null;
+    }
+
+    @Override
+    public Integer deleteStudent(Integer id) {
+        return null;
+    }
+}
