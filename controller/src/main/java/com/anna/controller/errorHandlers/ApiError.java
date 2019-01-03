15b407d01b66f300c2e9c019562b36025e63ed4b
AnnaGlshchnk <anna.glushchenko15@mail.ru>
2019-01-03 16:14:00 +0300

    add errorHandler

---
+++ controller/src/main/java/com/anna/controller/errorHandlers/ApiError.java
@@ -0,0 +1,38 @@
+package com.anna.controller.errorHandlers;
+
+import org.springframework.http.HttpStatus;
+
+public class ApiError {
+
+    private HttpStatus status;
+    private String message;
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public ApiError(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "ApiError{" +
+                "status=" + status +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
